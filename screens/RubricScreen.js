797912f9e1a07e923f4a3301990a7814d1f554c8
tahaiftikhar <t-w-i@hotmail.com>
2020-04-20 11:37:14 -0400

    Switched to storing in variables and simpler design

--- screens/RubricScreen.js
+++ screens/RubricScreen.js
@@ -10,7 +10,13 @@ import {
 } from 'react-native';
 
 function RubricScreen() {
-    const [choices, setChoices] = useState();
+    const [choices, setChoices] = useState([
+        {id: 0, description: "Default Choice"}
+        ]);
+
+    const [impDes, setImpDesc] = useState();
+
+
 
     var radio_props_implementation = [
         {label: "Poor", value: 0 },
@@ -19,7 +25,30 @@ function RubricScreen() {
         {label: "Excelent", value: 3 }
     ];
 
-    const onPress = (e) => {
+    //results are stored here
+    var c1,c2,c3;
+
+    const onPressImp = (e, category) => {
+        // setChoices({id:5, description: "test"})
+        if(category == "Implementation"){
+            c1=e;
+            console.log("Implementation: " + c1)
+        }
+        else if(category == "Design"){
+            c2=e;
+            console.log("Design choice: " + c2)
+        }
+        else if(category == "Delivery"){
+            c3=e;
+            console.log("Delivery choice: " + c3)
+        }
+        else{
+            console.log("Unknown Category selected")
+        }
+        // c1=e;
+        // console.log(category)
+        // console.log(e)
+        // console.log(choices.id)
 
         // ReactDOM.getElementById("1");
     };
@@ -28,50 +57,82 @@ function RubricScreen() {
         <SafeAreaView>
             <ScrollView style={styles.scrollView}>
             <View style={styles.innerContainer}>
-            <Text style={{marginTop: 20, fontSize: 20}}>Evaluation Rubric</Text>
+            <Text style={{marginTop: 20, fontSize: 20, fontWeight: "bold"}}>Evaluation Rubric</Text>
 
             <View style={styles.eval}>
-                <TouchableOpacity
-                    style={styles.selected}
-                    activeOpacity={0.6}
-                    underlayColor="#DDDDDD"
-                    onPress={onPress("1")}>
+                {/*<TouchableOpacity*/}
+                {/*    style={styles.selected}*/}
+                {/*    activeOpacity={0.6}*/}
+                {/*    underlayColor="#DDDDDD"*/}
+                {/*    onPress={onPressImp("1")}>*/}
                 <Text nativeID="1" style={styles.subHeading}>Implementation</Text>
-                </TouchableOpacity>
+                {/*</TouchableOpacity>*/}
+                <Text style={styles.description}>The the presenters has implemented all features</Text>
                 <RadioForm
                     radio_props={radio_props_implementation}
-                    initial={0}
+                    initial={1}
                     formHorizontal = {true}
                     labelHorizontal = {false}
-                    onPress={(value) => {setChoices(5)} } //works,
+                    // onPress={(value) => {setChoices(value)}} //works,
+                    onPress={(value) => {onPressImp(value, "Implementation")}} //works,
+                    // onPress={(value) => {this.setState({value:value})}}
+
                     labelStyle={styles.radio}
-                >
+                />
 
-                </RadioForm>
-                <Text style={styles.choices}><Text style={{color:"red"}}>Poor:</Text> The student did so andsnllklklklklklkklklo</Text>
-                <Text style={styles.choices}><Text style={{color:"orange"}}>Satisfactory:</Text> The student did so andsnllklklklklklkklklo</Text>
-                <Text style={styles.choices}><Text style={{color:"blue"}}>Good: </Text>The student did so andsnllklklklklklkklklo</Text>
-                <Text style={styles.choices}><Text style={{color:"green"}}>Excelent:</Text> The student did so andsnllklklklklklkklklo</Text>
-            </View>
-            <View style={styles.eval}>
-                <Text style={styles.subHeading}>Design</Text>
-                <Text style={styles.choices}><Text style={{color:"red"}}>Poor:</Text> The student did so andsnllklklklklklkklklo</Text>
-                <Text style={styles.choices}><Text style={{color:"orange"}}>Satisfactory:</Text> The student did so andsnllklklklklklkklklo</Text>
-                <Text style={styles.choices}><Text style={{color:"blue"}}>Good: </Text>The student did so andsnllklklklklklkklklo</Text>
-                <Text style={styles.choices}><Text style={{color:"green"}}>Excelent:</Text> The student did so andsnllklklklklklkklklo</Text>
+                    <Text style={styles.subHeading}>Design</Text>
+                <Text style={styles.description}>The the presentation meets design standards </Text>
+                <RadioForm
+                    radio_props={radio_props_implementation}
+                    initial={1}
+                    formHorizontal = {true}
+                    labelHorizontal = {false}
+                    // onPress={(value) => {setChoices(value)}} //works,
+                    onPress={(value) => {onPressImp(value, "Design")}} //works,
+                    // onPress={(value) => {this.setState({value:value})}}
 
-            </View>
-            <View style={styles.eval}>
+                    labelStyle={styles.radio}
+                />
+
                 <Text style={styles.subHeading}>Delivery</Text>
-                <Text style={styles.choices}><Text style={{color:"red"}}>Poor:</Text> The student did so andsnllklklklklklkklklo</Text>
-                <Text style={styles.choices}><Text style={{color:"orange"}}>Satisfactory:</Text> The student did so andsnllklklklklklkklklo</Text>
-                <Text style={styles.choices}><Text style={{color:"blue"}}>Good: </Text>The student did so andsnllklklklklklkklklo</Text>
-                <Text style={styles.choices}><Text style={{color:"green"}}>Excelent:</Text> The student did so andsnllklklklklklkklklo</Text>
+                <Text style={styles.description}>The presentation was delivered in a manner expected from college students  </Text>
+                <RadioForm
+                    radio_props={radio_props_implementation}
+                    initial={1}
+                    formHorizontal = {true}
+                    labelHorizontal = {false}
+                    // onPress={(value) => {setChoices(value)}} //works,
+                    onPress={(value) => {onPressImp(value, "Delivery")}} //works,
+                    // onPress={(value) => {this.setState({value:value})}}
+
+                    labelStyle={styles.radio}
+                />
+
+                {/*<Text style={styles.choices}><Text style={{color:"red"}}>Poor:</Text> {this.state.impDes}</Text>*/}
+                {/*<Text style={styles.choices}><Text style={{color:"red"}}>Poor:</Text> The student did so andsnllklklklklklkklklo</Text>*/}
+                {/*<Text style={styles.choices}><Text style={{color:"orange"}}>Satisfactory:</Text> The student did so andsnllklklklklklkklklo</Text>*/}
+                {/*<Text style={styles.choices}><Text style={{color:"blue"}}>Good: </Text>The student did so andsnllklklklklklkklklo</Text>*/}
+                {/*<Text style={styles.choices}><Text style={{color:"green"}}>Excelent:</Text> The student did so andsnllklklklklklkklklo</Text>*/}
             </View>
+            {/*<View style={styles.eval}>*/}
+            {/*    <Text style={styles.subHeading}>Design</Text>*/}
+            {/*    <Text style={styles.choices}><Text style={{color:"red"}}>Poor:</Text> The student did so andsnllklklklklklkklklo</Text>*/}
+            {/*    <Text style={styles.choices}><Text style={{color:"orange"}}>Satisfactory:</Text> The student did so andsnllklklklklklkklklo</Text>*/}
+            {/*    <Text style={styles.choices}><Text style={{color:"blue"}}>Good: </Text>The student did so andsnllklklklklklkklklo</Text>*/}
+            {/*    <Text style={styles.choices}><Text style={{color:"green"}}>Excelent:</Text> The student did so andsnllklklklklklkklklo</Text>*/}
+
+            {/*</View>*/}
+            {/*<View style={styles.eval}>*/}
+            {/*    <Text style={styles.subHeading}>Delivery</Text>*/}
+            {/*    <Text style={styles.choices}><Text style={{color:"red"}}>Poor:</Text> The student did so andsnllklklklklklkklklo</Text>*/}
+            {/*    <Text style={styles.choices}><Text style={{color:"orange"}}>Satisfactory:</Text> The student did so andsnllklklklklklkklklo</Text>*/}
+            {/*    <Text style={styles.choices}><Text style={{color:"blue"}}>Good: </Text>The student did so andsnllklklklklklkklklo</Text>*/}
+            {/*    <Text style={styles.choices}><Text style={{color:"green"}}>Excelent:</Text> The student did so andsnllklklklklklkklklo</Text>*/}
+            {/*</View>*/}
 
             <Button title={"Submit"}></Button>
         </View>
-            </ScrollView>
+            </ScrollView >
         </SafeAreaView>
     )
 }
@@ -80,8 +141,10 @@ function RubricScreen() {
 
 
 const styles = StyleSheet.create({
+    scrollView:{marginBottom:25},
     radio:{
-
+        marginTop: 10,
+        margin: 25,
     },
     innerContainer: {
         alignItems: "center",
@@ -92,17 +155,20 @@ const styles = StyleSheet.create({
     },
     subHeading:{
         fontSize: 20,
+        color: 'blue',
         textAlign: "left",
         paddingLeft: 20,
         paddingRight:20,
         marginBottom: 5
     },
-    choices:{
+    description:{
         fontSize: 20,
         textAlign: "left",
-        paddingLeft: 20,
-        paddingRight:20,
-        paddingTop:15,
+        padding: 20,
+        marginBottom: 10,
+        // paddingLeft: 20,
+        // paddingRight:20,
+        // paddingTop:15,
         backgroundColor: "#fdf5e6"
 
     },
